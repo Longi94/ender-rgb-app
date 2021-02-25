c6b63455233c6e370f61517b6827dcb7befb7285
Long Tran <lngtrn94@gmail.com>
2021-02-25 15:00:46 +0100

    remove code duplication

--- app/src/main/java/dev/longi/enderrgb/ui/lights/LightsViewModel.kt
+++ app/src/main/java/dev/longi/enderrgb/ui/lights/LightsViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.preference.PreferenceManager
 import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import dev.longi.enderrgb.R
 import dev.longi.enderrgb.network.VolleyQueue
@@ -28,6 +29,28 @@ class LightsViewModel(app: Application) : AndroidViewModel(app) {
     private val _effects = MutableLiveData<List<String>>()
     val effects: LiveData<List<String>> = _effects
 
+    private fun jsonRequest(method: Int, path: String, listener: Response.Listener<JSONObject>? = null, jsonBody: JSONObject? = null) {
+        try {
+            val url = url(path)
+            val request =
+                JsonObjectRequest(method, url, jsonBody,
+                    { response ->
+                        Log.i(TAG, "stopEffect: Request successful: $response")
+                        listener?.onResponse(response)
+                    },
+                    { error ->
+                        _error.value = error.message
+                        Log.e(TAG, "Failed to send request to $url", error)
+                    }
+                )
+
+            VolleyQueue.getInstance(getApplication()).addToRequestQueue(request)
+        } catch (e: Exception) {
+            _error.value = e.message
+            Log.e(TAG, "exception while making request to $path", e)
+        }
+    }
+
     fun setColor(color: Int) {
         val b = color and 255
         val g = (color shr 8) and 255
@@ -41,22 +64,7 @@ class LightsViewModel(app: Application) : AndroidViewModel(app) {
         body.put("g", g)
         body.put("b", b)
 
-        try {
-            val request = JsonObjectRequest(Request.Method.POST, url("light/rgb"), body,
-                { response ->
-                    Log.i(TAG, "setColor: Request successful: $response")
-                },
-                { error ->
-                    _error.value = error.message
-                    Log.e(TAG, "setColor: Failed to send request", error)
-                }
-            )
-
-            VolleyQueue.getInstance(getApplication()).addToRequestQueue(request)
-        } catch (e: Exception) {
-            _error.value = e.message
-            Log.e(TAG, "setColor: exception while making request", e)
-        }
+        jsonRequest(Request.Method.POST, "light/rgb", null, body)
     }
 
     fun off() {
@@ -64,33 +72,19 @@ class LightsViewModel(app: Application) : AndroidViewModel(app) {
     }
 
     fun getEffects() {
-        try {
-            val request = JsonObjectRequest(Request.Method.GET, url("light/effect"), null,
-                { response ->
-                    Log.i(TAG, "getEffects: Request successful: $response")
-                    try {
-                        val effectsArray = response.getJSONArray("response")
-                        val effects = mutableListOf<String>()
-                        for (i in 0 until effectsArray.length()) {
-                            effects.add(effectsArray.getString(i))
-                        }
-                        _effects.value = effects
-                    } catch (e: JSONException) {
-                        _effects.value = mutableListOf()
-                        Log.e(TAG, "getEffects: Malformed JSON, $response", e)
-                    }
-                },
-                { error ->
-                    _error.value = error.message
-                    Log.e(TAG, "getEffects: Failed to send request", error)
+        jsonRequest(Request.Method.GET, "light/effect", { response ->
+            try {
+                val effectsArray = response.getJSONArray("response")
+                val effects = mutableListOf<String>()
+                for (i in 0 until effectsArray.length()) {
+                    effects.add(effectsArray.getString(i))
                 }
-            )
-
-            VolleyQueue.getInstance(getApplication()).addToRequestQueue(request)
-        } catch (e: Exception) {
-            _error.value = e.message
-            Log.e(TAG, "getEffects: exception while making request", e)
-        }
+                _effects.value = effects
+            } catch (e: JSONException) {
+                _effects.value = mutableListOf()
+                Log.e(TAG, "getEffects: Malformed JSON, $response", e)
+            }
+        })
     }
 
     private fun url(path: String): String {
@@ -108,42 +102,10 @@ class LightsViewModel(app: Application) : AndroidViewModel(app) {
     }
 
     fun setEffect(effectName: String) {
-        try {
-            val request =
-                JsonObjectRequest(Request.Method.POST, url("light/effect/$effectName"), null,
-                    { response ->
-                        Log.i(TAG, "setEffect: Request successful: $response")
-                    },
-                    { error ->
-                        _error.value = error.message
-                        Log.e(TAG, "setEffect: Failed to send request", error)
-                    }
-                )
-
-            VolleyQueue.getInstance(getApplication()).addToRequestQueue(request)
-        } catch (e: Exception) {
-            _error.value = e.message
-            Log.e(TAG, "setEffect: exception while making request", e)
-        }
+        jsonRequest(Request.Method.POST, "light/effect/$effectName")
     }
 
     fun stopEffect() {
-        try {
-            val request =
-                JsonObjectRequest(Request.Method.DELETE, url("light/effect"), null,
-                    { response ->
-                        Log.i(TAG, "stopEffect: Request successful: $response")
-                    },
-                    { error ->
-                        _error.value = error.message
-                        Log.e(TAG, "stopEffect: Failed to send request", error)
-                    }
-                )
-
-            VolleyQueue.getInstance(getApplication()).addToRequestQueue(request)
-        } catch (e: Exception) {
-            _error.value = e.message
-            Log.e(TAG, "stopEffect: exception while making request", e)
-        }
+        jsonRequest(Request.Method.DELETE, "light/effect")
     }
 }
